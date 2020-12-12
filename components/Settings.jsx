752bfa58f1633cf000ea0aa9346cae733884ea9f
abUwUser <pedro.santos.cartaxo@gmail.com>
2020-12-12 04:56:45 -0300

    settings updated, also sry for using true and false, don't wanna reset this setting for everyone else

--- components/Settings.jsx
+++ components/Settings.jsx
@@ -1,14 +1,25 @@
 const { React } = require('@vizality/webpack')
-const { settings: { SwitchItem } } = require('@vizality/components')
+const { settings: { SwitchItem, RadioGroup } } = require('@vizality/components')
 
-module.exports = ({ getSetting, toggleSetting }) => <>
-    <SwitchItem
+module.exports = ({ getSetting, toggleSetting, updateSetting }) => <>
+    {/* <SwitchItem
         value={getSetting('collapsibleActivities')}
         onChange={() => toggleSetting('collapsibleActivities')}
         note='Show activities as collapsible "categories" (only works with ≥2 activities)'
-    >Collapsible Activities</SwitchItem>
+    >Collapsible Activities</SwitchItem> */}
+    <RadioGroup
+        options={[
+            { name: "Buttons", value: false },
+            { name: "Collapsible Activities", value: true }
+        ]}
+        value={getSetting('collapsibleActivities', false)}
+        onChange={e => {
+            updateSetting('collapsibleActivities', e.value)
+            this.forceUpdate();
+        }}
+    > Display method </RadioGroup>
     <SwitchItem
-        value={getSetting('autoOpen')}
+        value={getSetting('autoOpen') && getSetting('collapsibleActivities')}
         onChange={() => toggleSetting('autoOpen')}
         disabled={!getSetting('collapsibleActivities')}
     >Auto open activities</SwitchItem>
